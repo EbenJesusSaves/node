77eed446fddf2d67f373fbcd2b718179020714a4
Eben <agyekumebenezer2001@gmail.com>
2024-02-17 21:13:20 +0000

    written some routes

--- src/handlers/products.ts
+++ src/handlers/products.ts
@@ -15,13 +15,38 @@ export const getAllProducts = async (req, res) => {
 };
 
 export const addProduct = async (req, res) => {
-  const user = await prisma.user.findUnique({
-    where: { id: req.user.id },
-  });
+  // const user = await prisma.user.findUnique({
+  //   where: { id: req.user.id },
+  // });
   const product = await prisma.product.create({
     data: {
       name: req.body.name,
-      belongsTo: user.id,
+      belongToId: req.user.id,
     },
   });
+  res.status(200);
+  res.json({ data: product });
+};
+
+export const updateProduct = async (req, res) => {
+  const update = await prisma.product.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      name: req.body.name,
+    },
+  });
+  res.json({ data: update, message: "Product updated " });
+  res.status(200);
+};
+
+export const deleteProduct = async (req, res) => {
+  await prisma.product.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.json({ message: "Product deleted " });
+  res.status(200);
 };
